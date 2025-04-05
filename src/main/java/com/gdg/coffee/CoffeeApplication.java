e520769dc9ac466a0e5a6619c95f8e56a3aacb8e
jh011007@naver.com <jihyeon@jihyeonui-MacBookPro.local>
2025-04-05 22:06:39 +0900

    [feat] Add custom error response codes for Member and enable JPA auditing

--- src/main/java/com/gdg/coffee/CoffeeApplication.java
+++ src/main/java/com/gdg/coffee/CoffeeApplication.java
@@ -2,7 +2,9 @@ package com.gdg.coffee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CoffeeApplication {
 
