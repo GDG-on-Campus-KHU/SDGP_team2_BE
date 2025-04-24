166ffd150621d5cd61fe09f17b8a0946243c2c09
gleaming9 <gleaming9@khu.ac.kr>
2025-04-24 21:45:30 +0900

    [Feat] enable DTO validation and add global BAD_REQUEST handlers

--- src/main/java/com/gdg/coffee/global/common/response/GlobalResponseAdvice.java
+++ src/main/java/com/gdg/coffee/global/common/response/GlobalResponseAdvice.java
@@ -2,7 +2,9 @@ package com.gdg.coffee.global.common.response;
 
 import com.gdg.coffee.global.common.exception.BaseException;
 import com.gdg.coffee.global.common.type.ErrorResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +18,23 @@ public class GlobalResponseAdvice {
                 e.getHttpStatus()
         );
 
+    }
+
+    // DTO 유효성 검사 실패 시 모두 global BAD_REQUEST로 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidation(MethodArgumentNotValidException ex) {
+        ApiResponse<?> body = ApiResponse.error(ErrorCode.BAD_REQUEST);
+        return ResponseEntity
+                .status(ErrorCode.BAD_REQUEST.getHttpStatus())
+                .body(body);
+    }
+
+    // DB 제약조건 위반 시 모두 global BAD_REQUEST로 처리
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiResponse<?>> handleDb(DataIntegrityViolationException ex) {
+        ApiResponse<?> body = ApiResponse.error(ErrorCode.BAD_REQUEST);
+        return ResponseEntity
+                .status(ErrorCode.BAD_REQUEST.getHttpStatus())
+                .body(body);
     }
 }
