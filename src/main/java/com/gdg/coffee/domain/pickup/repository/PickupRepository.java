b8045b00bc2c35c383eae662dc99553e05ab0ec3
jh011007@naver.com <jh011007@naver.com>
2025-05-02 10:08:08 +0900

    [Feat] Implement Pickup service, controller, repository

---
+++ src/main/java/com/gdg/coffee/domain/pickup/repository/PickupRepository.java
@@ -0,0 +1,39 @@
+package com.gdg.coffee.domain.pickup.repository;
+
+import com.gdg.coffee.domain.pickup.domain.Pickup;
+import com.gdg.coffee.domain.pickup.dto.PickupCafeSummaryDto;
+import com.gdg.coffee.domain.pickup.dto.PickupStatus;
+import com.gdg.coffee.domain.pickup.dto.PickupUserSummaryDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PickupRepository extends JpaRepository<Pickup, Long> {
+    List<Pickup> findByGround_Cafe_Id(Long cafeId);
+
+    @Query("SELECT new com.gdg.coffee.domain.pickup.dto.PickupCafeSummaryDto(" +
+            "p.id, m.username, p.pickupDate, p.createdDate, b.name, p.amount, p.status) " +
+            "FROM Pickup p " +
+            "JOIN p.member m " +
+            "JOIN p.ground g " +
+            "JOIN g.bean b " +
+            "WHERE g.cafe.id = :cafeId AND p.status = :status")
+    List<PickupCafeSummaryDto> findCafePickupListByStatus(@Param("cafeId") Long cafeId,
+                                                          @Param("status") PickupStatus status);
+
+    @Query("SELECT new com.gdg.coffee.domain.pickup.dto.PickupUserSummaryDto(" +
+            "p.id, c.name, p.pickupDate, p.createdDate, b.name, p.amount, p.status) " +
+            "FROM Pickup p " +
+            "JOIN p.ground g " +
+            "JOIN g.cafe c " +
+            "JOIN g.bean b " +
+            "WHERE p.member.id = :userId AND p.status = :status")
+    List<PickupUserSummaryDto> findUserPickupListByStatus(@Param("userId") Long userId,
+                                                          @Param("status") PickupStatus status);
+
+    List<Pickup> findByMember_Id(Long memberId);
+
+}
